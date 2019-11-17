cd321e9f469f1e741f681114402a844e35109344
Magda Ambrozy <magda.ambrozy@gmail.com>
2019-11-17 20:06:22 +0100

    Add single-post page

--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -3,9 +3,12 @@ const router = express.Router();
 
 const PostController = require('../controllers/post.controller');
 
+const { getPosts, getSinglePost } = PostController;
+
 // get all posts
-router.route('/posts').get(PostController.getPosts);
+router.route('/posts').get(getPosts);
 
-
+// get single post
+ router.route('/posts/:post_id').get(getSinglePost);
 
 module.exports = router;
