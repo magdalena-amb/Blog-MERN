8ab8ebff6893690ad2aaf71b0f49568262359832
Magda Ambrozy <magda.ambrozy@gmail.com>
2019-11-25 09:47:39 +0100

    Modify DB in config.js

--- server/config.js
+++ server/config.js
@@ -1,5 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
-    DB: process.env.MONGO_URI || 'mongodb+srv://magdalena_a:F6MnJhPelz38JdVW@cluster0-iamgj.mongodb.net/test?retryWrites=true&w=majority',
+    DB: process.env.MONGO_URI || 'mongodb://localhost:27017/mernapp',
   };
+
+  // 'mongodb+srv://magdalena_a:F6MnJhPelz38JdVW@cluster0-iamgj.mongodb.net/test?retryWrites=true&w=majority',
   
